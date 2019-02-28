c004ceb5df7bc82caaa1b83c9d69992579f6a334
515hikaru <12kojima.takahiro@gmail.com>
2019-02-28 14:06:57 +0900

    Set default directry to ghq root

--- init.el
+++ init.el
@@ -146,6 +146,17 @@
   :config
   (setq neo-show-hidden-files t)
   (setq neo-smart-open t))
+(use-package subr-x)
+(defun get--ghq-directory-path()
+  "Get ghq root path. if there is not ghq, return HOME directory"
+  (if (executable-find "ghq")
+    (string-trim (shell-command-to-string "ghq root | tail -n 1"))
+    (getenv "HOME")))
+(defun open-ghq-root()
+    "Open ghq root directory with dired."
+    (interactive)
+    (dired (get--ghq-directory-path)))
+(setq default-directory (get--ghq-directory-path))
 ;;; auto config
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -155,7 +166,7 @@
  '(conda-anaconda-home "~/miniconda3")
  '(package-selected-packages
    (quote
-    (neotree elixir-mode dockerfile-mode toml-mode yaml-mode julia-repl flycheck-julia julia-mode conda ein go-mode yasnippet lsp-ui python-mode company-lsp lsp-mode markdown-mode racer flycheck-rust exec-path-from-shell company-racer rust-mode magit open-junk-file flycheck-elm flycheck company use-package atom-one-dark-theme org-plus-contrib elm-mode))))
+    (subr-x neotree elixir-mode dockerfile-mode toml-mode yaml-mode julia-repl flycheck-julia julia-mode conda ein go-mode yasnippet lsp-ui python-mode company-lsp lsp-mode markdown-mode racer flycheck-rust exec-path-from-shell company-racer rust-mode magit open-junk-file flycheck-elm flycheck company use-package atom-one-dark-theme org-plus-contrib elm-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
