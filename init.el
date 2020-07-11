33a8c1dcdd1febfe038135f1cf061015ad449680
515hikaru <12kojima.takahiro@gmail.com>
2020-07-12 01:59:27 +0900

    custom doom-modeline-mode

--- init.el
+++ init.el
@@ -59,7 +59,19 @@
   (doom-themes-org-config))
 (use-package doom-modeline
   :ensure t
-  :init (doom-modeline-mode 1))
+  :init (doom-modeline-mode 1)
+  :custom (doom-modeline-buffer-file-name-style 'truncate-with-project)
+  (doom-modeline-icon t)
+  (doom-modeline-major-mode-icon nil)
+  (doom-modeline-minor-modes nil)
+  :hook
+  (after-init . doom-modeline-mode)
+  :config
+  (line-number-mode 0)
+  (column-number-mode 0)
+  (doom-modeline-def-modeline 'main
+    '(bar window-number buffer-info remote-host buffer-position parrot selection-info)
+    '(misc-info persp-name lsp github debug minor-modes input-method major-mode process vcs checker)))
 (use-package rainbow-delimiters
   :ensure t
   :init (rainbow-delimiters-mode 1)
