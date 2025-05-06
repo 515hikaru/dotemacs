a5011b6ac12e67d48dc66a3d27a4cfac8595d91c
Takahiro KOJIMA <12kojima.takahiro@gmail.com>
2025-05-06 19:24:40 +0900

    update latest weekly review templates

--- init.el
+++ init.el
@@ -40,8 +40,7 @@
 (use-package consult
   :bind (("C-s" . consult-line)
          ("M-y" . consult-yank-pop)))
-
-
+;; org-mode
 (use-package org-roam
   :custom
   (org-roam-directory (file-truename "/Users/hikaru/Library/Mobile Documents/com~apple~CloudDocs/Documents/zettelkasten"))
@@ -56,7 +55,7 @@
 (defun my/new-draft ()
   "Create a new draft org file with a timestamped filename and title, including Japanese."
   (interactive)
-  (let* ((draft-dir "/Users/hikaru/Library/Mobile Documents/com~apple~CloudDocs/Documents/zettelkasten/drafts/")
+  (let* ((draft-dir "~/Library/Mobile Documents/com~apple~CloudDocs/Documents/zettelkasten/drafts/")
          (title (read-string "Draft title: "))
          ;; スペースだけアンダーバー、それ以外はそのまま
          (sanitized-title (replace-regexp-in-string " " "_" title))
@@ -70,7 +69,7 @@
 (defun open-weekly-reviews-file ()
   "Weekly Reviewsファイルを開く"
   (interactive)
-  (let ((file-name "/Users/hikaru/Library/Mobile Documents/com~apple~CloudDocs/Documents/weekly_reviews/2025-weekly-reviews.org"))
+  (let ((file-name "~/Library/Mobile Documents/com~apple~CloudDocs/Documents/weekly_reviews/2025-weekly-reviews.org"))
     (find-file file-name)
     (when (= (buffer-size) 0)
       (insert "#+TITLE: Weekly Reviews\n")
@@ -90,10 +89,10 @@
                                                       (days-to-time (string-to-number (format-time-string "%u" time))))))
          (week-end (format-time-string "%Y-%m-%d"
                                       (time-add time
-                                               (days-to-time (- 7 (string-to-number (format-time-string "%u" time))))))))
+                                               (days-to-time (- 6 (string-to-number (format-time-string "%u" time))))))))
     (insert (format "\n* %s-W%s (%s 〜 %s)\n" year week week-start week-end))
-    (insert "📌** 1. What Happened\n**（今週の出来事・やったこと、事実ベース）**\n- \n\n")
-    (insert "🌟** 2. What Went Well\n**（よかったこと・うまくいったこと）**\n- \n\n")
-    (insert "💥** 3. What Didn't Go Well\n**（しんどかったこと・詰まったこと）**\n- \n\n")
-    (insert "🔁** 4. What to Adjust\n**（来週改善したいこと・やめる／始める）**\n- \n\n")
-    (insert "🌱** 5. Still Thinking...\n**（もやもや・未整理・考え中のこと）**\n- \n")))
+    (insert "** 1. What Happened （今週の出来事・やったこと、事実ベース）\n- \n\n")
+    (insert "** 2. What Went Well （よかったこと・うまくいったこと）\n- \n\n")
+    (insert "** 3. What Didn't Go Well （しんどかったこと・詰まったこと）\n- \n\n")
+    (insert "** 4. What to Adjust\n （来週改善したいこと・やめる／始める）\n- \n\n")
+    (insert "** 5. Still Thinking... （もやもや・未整理・考え中のこと）\n- \n")))
