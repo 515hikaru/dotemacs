07dc6842a4e951a9874b6db337be5a4cc224c172
515hikaru <kojima.takahiro@hey.com>
2025-08-17 13:36:03 +0900

    update wsl-init

--- init-wsl.el
+++ init-wsl.el
@@ -10,3 +10,10 @@
 (set-face-attribute 'default nil
                     :family "IPAexGothic"
                     :height 280)
+(set-fontset-font "fontset-default" '(#x3000 . #x303F) "IPAexGothic" nil 'prepend)
+(set-fontset-font "fontset-default" 'japanese-jisx0208 "IPAexGothic" nil 'prepend)
+(set-fontset-font "fontset-default" 'japanese-jisx0212 "IPAexGothic" nil 'prepend)
+(set-fontset-font "fontset-default" 'japanese-jisx0213-1 "IPAexGothic" nil 'prepend)
+(set-fontset-font "fontset-default" 'japanese-jisx0213-2 "IPAexGothic" nil 'prepend)
+(set-fontset-font "fontset-default" 'han "IPAexGothic" nil 'prepend)
+(set-fontset-font "fontset-default" 'kana "IPAexGothic" nil 'prepend)
